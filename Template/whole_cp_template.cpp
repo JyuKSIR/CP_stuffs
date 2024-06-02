fadf48cce8ca2c805d0af279f53d51c1e7d320d2
JyuKSIR <162912251+JyuKSIR@users.noreply.github.com>
2024-06-02 16:46:58 +0600

    Update whole_cp_template.cpp

--- Template/whole_cp_template.cpp
+++ Template/whole_cp_template.cpp
@@ -69,3 +69,73 @@ int main(){
 
   return 0;
 }
+
+
+
+
+
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+const int N = 10e5+5;
+vector<vector<int>> graph(N);
+vector<int> visited(N);
+
+void bfs(int start_node){
+   queue<int> q;
+
+   q.push(start_node);
+   visited[start_node]=1;
+
+   while(!q.empty()){
+     int first = q.front();
+     cout << first << "\n";
+     q.pop();
+     //cout << first << "\n";
+     for(int b : graph[start_node]){
+        if(visited[b]==0){
+            q.push(b);
+            visited[b]=1;
+        }
+     }
+
+   }
+}
+
+int main(){
+    int nodes;
+    cin >> nodes;
+    for(int i=0; i<nodes-1; i++){
+        int x,y;
+        cin >> x >> y;
+        graph[x].push_back(y);
+        graph[y].push_back(x);
+    }
+    vector<int> input(nodes);
+
+    for(int i=0; i<nodes; i++){
+        cin >> input[i];
+    }
+    int temp = input[0];
+
+    for(int i=0; i<nodes; i++){
+        if(temp > input[i]){
+            temp = input[i];
+        }
+    }
+    cout << temp << "\n";
+    bfs(temp);
+
+    for(int i=0; i<nodes; i++){
+        cout << "node: " << i+1 << "\n";
+        cout << "child nodes: ";
+        for(int a : graph[i]){
+            cout << a << " ";
+        }
+        cout << "\n";
+    }
+
+}
+
