14879db8b985ebd66ec62bf56fd680d32a7d22d7
JyuKSIR <162912251+JyuKSIR@users.noreply.github.com>
2024-06-11 12:33:21 +0600

    Create basic.cpp

---
+++ Template/basic.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define dbg(a) cerr<<__LINE__<<" no. line: "<<#a<<" = "<<a<<endl
+
+void solve(){
+
+    
+
+}
+
+int main(){
+
+  #ifndef ONLINE_JUDGE
+   freopen("input.txt", "r", stdin);
+   freopen("output.txt", "w", stdout);
+  #endif
+
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+
+  int t;
+  cin>>t;
+  while(t--){
+    solve();
+  }
+
+  return 0;
+}
