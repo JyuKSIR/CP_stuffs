417caa4562b48ba80c553f3b114cd8c635a3515c
JyuKSIR <162912251+JyuKSIR@users.noreply.github.com>
2024-06-15 14:14:44 +0600

    Update dfs_in_graph.cpp

--- Graph,Tree/Traversal/dfs_in_graph.cpp
+++ Graph,Tree/Traversal/dfs_in_graph.cpp
@@ -1,5 +1,73 @@
 //depth_first_search in graph
 //https://www.youtube.com/watch?v=9_ftWKch6vI&list=PLauivoElc3ghxyYSr_sVnDUc_ynPk6iXE&index=4-references
+//**input**
+// 7 12        
+// 0 1
+// 0 6
+// 0 2
+// 2 3
+// 2 6
+// 6 4
+// 1 6
+// 1 4
+// 1 5
+// 4 5
+// 4 3
+// 5 3
+//**output**
+// parent : 0
+// child: 
+// 1 6 2 
+// parent : 1
+// child: 
+// 0 6 4 5 
+// parent : 2
+// child: 
+// 0 3 6 
+// parent : 3
+// child: 
+// 2 4 5 
+// parent : 4
+// child: 
+// 6 1 5 3 
+// parent : 5
+// child: 
+// 1 4 3 
+// parent : 6
+// child: 
+// 0 2 4 1 
+// dfs order: 0
+// parent: 0 child: 1
+// dfs order: 1
+// parent: 1 child: 0
+// parent: 1 child: 6
+// dfs order: 6
+// parent: 6 child: 0
+// parent: 6 child: 2
+// dfs order: 2
+// parent: 2 child: 0
+// parent: 2 child: 3
+// dfs order: 3
+// parent: 3 child: 2
+// parent: 3 child: 4
+// dfs order: 4
+// parent: 4 child: 6
+// parent: 4 child: 1
+// parent: 4 child: 5
+// dfs order: 5
+// parent: 5 child: 1
+// parent: 5 child: 4
+// parent: 5 child: 3
+// parent: 4 child: 3
+// parent: 3 child: 5
+// parent: 2 child: 6
+// parent: 6 child: 4
+// parent: 6 child: 1
+// parent: 1 child: 4
+// parent: 1 child: 5
+// parent: 0 child: 6
+// parent: 0 child: 2
+
 #include<bits/stdc++.h>
 using namespace std;
 
