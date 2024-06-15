d1eeb06d909d7bb0897bf6c6d3afb46505aeeab2
JyuKSIR <162912251+JyuKSIR@users.noreply.github.com>
2024-06-15 23:00:09 +0600

    Update dfs_in_graph.cpp

--- Graph,Tree/Traversal/dfs_in_graph.cpp
+++ Graph,Tree/Traversal/dfs_in_graph.cpp
@@ -16,45 +16,38 @@
 // 5 3
 //**output**
 // parent : 0
-// child: 
-// 1 6 2 
+// child: 1 6 2 
 // parent : 1
-// child: 
-// 0 6 4 5 
+// child: 0 6 4 5 
 // parent : 2
-// child: 
-// 0 3 6 
+// child: 0 3 6 
 // parent : 3
-// child: 
-// 2 4 5 
+// child: 2 4 5 
 // parent : 4
-// child: 
-// 6 1 5 3 
+// child: 6 1 5 3 
 // parent : 5
-// child: 
-// 1 4 3 
+// child: 1 4 3 
 // parent : 6
-// child: 
-// 0 2 4 1 
-// dfs order: 0
+// child: 0 2 4 1 
+// order: 0
 // parent: 0 child: 1
-// dfs order: 1
+// order: 1
 // parent: 1 child: 0
 // parent: 1 child: 6
-// dfs order: 6
+// order: 6
 // parent: 6 child: 0
 // parent: 6 child: 2
-// dfs order: 2
+// order: 2
 // parent: 2 child: 0
 // parent: 2 child: 3
-// dfs order: 3
+// order: 3
 // parent: 3 child: 2
 // parent: 3 child: 4
-// dfs order: 4
+// order: 4
 // parent: 4 child: 6
 // parent: 4 child: 1
 // parent: 4 child: 5
-// dfs order: 5
+// order: 5
 // parent: 5 child: 1
 // parent: 5 child: 4
 // parent: 5 child: 3
@@ -67,6 +60,7 @@
 // parent: 1 child: 5
 // parent: 0 child: 6
 // parent: 0 child: 2
+// dfs order: 0 1 6 2 3 4 5 
 
 #include<bits/stdc++.h>
 using namespace std;
@@ -77,18 +71,26 @@ vector<int> graph[N];
 
 int vis[N];
 
+vector<int> dfs_order;
+
 void dfs_function(int node){
-    cout<<node<<"\n"; //order of dfs
+    cout << "order: " <<node << "\n";
+    dfs_order.push_back(node);
     vis[node]=1;
     for(int child : graph[node]){
         cout<<"parent: "<<node<<" child: "<<child<<"\n";
         if(vis[child]!=1){
-            dfs_function(child); //keep calling the function till the nodes are visited
+            dfs_function(child);
         }  
     }
 }
 
 int main(){
+
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
     //taking input and output of graph;
     int nodes,edges;
     cin>>nodes>>edges;
@@ -100,7 +102,7 @@ int main(){
       graph[node2].push_back(node1);
     }
     for(int i=0; i<nodes; ++i){
-        cout << "parent : " <<i << "\n" <<"child: " << "\n";
+        cout << "parent : " <<i << "\n" <<"child: ";
         for(int x : graph[i]){
             cout<<x<<" ";
         }
@@ -109,6 +111,9 @@ int main(){
     //Depth first search in the graph;
     //let the dfs in graph starts with node: 0
     dfs_function(0);
-
+    cout << "dfs order: ";
+    for(int y : dfs_order){
+        cout << y << " ";
+    }
     return 0;
 }
