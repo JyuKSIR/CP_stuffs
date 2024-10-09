e6e9b82f567ea0500551d71d83c3e8b80dae58ee
JyuKSIR <162912251+JyuKSIR@users.noreply.github.com>
2024-10-10 00:35:07 +0600

    Add files via upload

---
+++ Number Theory/Problems/Euclid/EhAb_aNd_gCd.cpp
@@ -0,0 +1,29 @@
+// https://codeforces.com/contest/1325/problem/A
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define dbg(a) cerr<<__LINE__<<" no. line: "<<#a<<" = "<<a<<endl
+
+void solve(){
+
+    int n; cin >> n;
+
+    cout << 1 << " " << n-1 << "\n";
+
+}
+
+int main(){
+
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int t;
+    cin>>t;
+    while(t--){
+      solve();
+    }
+
+  return 0;
+}
