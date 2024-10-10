9c97fa0a28a984d891b8c46711dde5e022b7cc62
JyuKSIR <162912251+JyuKSIR@users.noreply.github.com>
2024-10-10 17:15:14 +0600

    Update phi_function.cpp

--- Number Theory/phi_function.cpp
+++ Number Theory/phi_function.cpp
@@ -14,13 +14,13 @@ int phi[N];
 
 void totient_by_prime(){
     
-    for(int i=1; i< N; i++){
+    for(int i=0; i<= N; i++){
         phi[i] = i;
     }
 
-    for(int i = 2; i < N; i++){
+    for(int i = 2; i <= N; i++){
         if(phi[i] == i){
-           for(int j = i; j < N; j += i){
+           for(int j = i; j <= N; j += i){
             //cout << "i: " << i << " " <<phi[j] << "\n";
             phi[j] -= phi[j]/i;  // when you divide 6 by 2 and the ans is 3 it means there are 3 multiple of 2 between 1 and 6 which are
                                  // 2,4,6 and it means gcd(2,6),gcd(4,6),gcd(6,6) can't be 1 so we are minusing 3 from 6 
@@ -29,7 +29,7 @@ void totient_by_prime(){
         }
     }
 
-    for(int i = 2; i<1000; i++){
+    for(int i = 2; i<=1000; i++){
         cout << phi[i] << " ";
     }
 
