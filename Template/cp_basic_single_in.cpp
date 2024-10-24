464b02acaa4dec8a2349ab71625d0f92827716e3
JyuKSIR <162912251+JyuKSIR@users.noreply.github.com>
2024-10-24 13:36:33 +0600

    Create cp_basic_single_in.cpp

---
+++ Template/cp_basic_single_in.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ld long double
+#define dbg(a) cerr<<__LINE__<<" no. line: "<<#a<<" = "<<a<<endl
+
+void solve(){
+
+    
+
+}
+
+int main(){
+
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+  
+       solve();
+
+  return 0;
+}
