59d6239ad131a0fa6813ff7846c85772f7c4a014
JyuKSIR <162912251+JyuKSIR@users.noreply.github.com>
2024-10-24 13:35:38 +0600

    Create cp_basic_multi_in.cpp

---
+++ Template/cp_basic_multi_in.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ld long double
+#define dbg(a) cerr<<__LINE__<<" no. line: "<<#a<<" = "<<a<<endl
+
+void solve(){
+
+    
+
+}
+
+int main(){
+
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int t; cin >> t;
+
+    while(t--){
+       solve();
+    }
+
+  return 0;
+}
