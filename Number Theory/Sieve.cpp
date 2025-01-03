10c2446379bf1379e094bd578b0cc77a11cfe153
JyuKSIR <162912251+JyuKSIR@users.noreply.github.com>
2025-01-04 02:12:10 +0600

    Update Sieve.cpp

--- Number Theory/Sieve.cpp
+++ Number Theory/Sieve.cpp
@@ -6,9 +6,9 @@ vector<bool> isPrime(N,true);
 
 int main(){
     isPrime[0] = isPrime[1] = false;
-    for(int i=2; i<N; i++){
+    for(int i=2; i*i<=N; i++){
         if(isPrime[i] == true){
-            for(int j=2*i; j<N; j+=i){
+            for(int j=2*i; j<=N; j+=i){
                 isPrime[j] = false;
             }
         }
