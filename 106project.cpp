471d6bdb64c6d4e26e8f8de985339e3d17bf334c
JyuKSIR <162912251+JyuKSIR@users.noreply.github.com>
2025-01-20 12:54:33 +0600

    Update 106project.cpp

--- 106project.cpp
+++ 106project.cpp
@@ -1,115 +1,50 @@
-#include <iostream>
-#include <cstdlib>
-#include <ctime>
-
-using namespace std;
-
-const int n = 5000;
-int ar[n+1][n+1];
-int deg[n+1];
-
-int main() {
-    srand(time(0));
-
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= n; j++) {
-            if(i!=j) ar[i][j] = ar[j][i] =  rand() % 2;
-        }
-    }
-
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= n; j++) {
-            //cout << ar[i][j] << " ";
-        }
-        //cout << "\n";
-    }
-
-    long long edge = 0;
-    for (int i = 1; i <= n; i++) {
-        for (int j = i+1; j <= n; j++) {
-            if (ar[i][j] == 1) {
-                edge++;
-            }
-        }
-    }
-    long long sumdeg = 0;
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= n; j++) {
-            if (ar[i][j] == 1) {
-                deg[i]++;
-            }
-        }
-        sumdeg += deg[i];
-    }
-
-    cout << "\nnumber of edges: " << edge << endl;
-    cout << "degrees of vertices:" << "\n";
-    for (int i = 1; i <= n; ++i) {
-        //cout << "vertex " << i << ": " << deg[i] << endl;
-    }
-    cout << "sum of degrees: " << sumdeg << endl;
-
-    return 0;
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
 #define N 5000
 
-int ar[N + 1][N + 1];
-int deg[N + 1];
+int ar[N+1][N+1];
 
 int main() {
+    
+	clock_t start_time = clock();
+
     srand(time(0));
 
-    for (int i = 1; i <= N; i++) {
-        for (int j = 1; j <= N; j++) {
-            if (i != j) {
-                ar[i][j] = ar[j][i] = rand() % 2;
+    for (int i=1; i<=N; i++) {
+        for (int j=i; j<=N; j++) {
+                ar[i][j] = ar[j][i] = rand() % 6;
+        }
+    }
+
+    long long edge = 0,sumdeg = 0;
+
+    for (int i=1; i<=N; i++) {
+        for (int j=1; j<=N; j++) {
+            if (ar[i][j]){
+                if (i == j) {
+                    edge += ar[i][j];
+                    sumdeg += 2 * ar[i][j];
+                }
+				else if(i<j){
+                    edge += ar[i][j];
+                }
+                if(i!=j) sumdeg+=ar[i][j];
             }
         }
     }
 
-    long long edge = 0;
-    for (int i = 1; i <= N; i++) {
-        for (int j = i + 1; j <= N; j++) {
-            if (ar[i][j] == 1) {
-                edge++;
-            }
-        }
-    }
-    long long sumdeg = 0;
-    for (int i = 1; i <= N; i++) {
-        deg[i] = 0;
-        for (int j = 1; j <= N; j++) {
-            if (ar[i][j] == 1) {
-                deg[i]++;
-            }
-        }
-        sumdeg += deg[i];
-    }
-    printf("\nNumber of edges: %lld\n", edge);
+    printf("Number of edges: %lld\n", edge);
     printf("Sum of degrees: %lld\n", sumdeg);
+
+	(sumdeg == 2*edge) ? printf("Handshaking theorum holds\n") : printf("Handshaking theorum didn't hold\n");
+
+	clock_t end_time = clock();
+
+    double execution_time_ms = ((double)(end_time - start_time) / CLOCKS_PER_SEC) * 1000;
+
+    printf("Execution time: %.3f ms\n", execution_time_ms);
 
     return 0;
 }
