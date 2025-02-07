c209b93a77b6b9c1cd4076b7d22191e122ed5d1e
JyuKSIR <162912251+JyuKSIR@users.noreply.github.com>
2025-02-07 21:30:28 +0600

    Update Dice_Combinations.cpp

--- Dynamic Programming/CSES/Dice_Combinations.cpp
+++ Dynamic Programming/CSES/Dice_Combinations.cpp
@@ -10,7 +10,7 @@ void solve() {
     vector<int> dp(n+1, 0);
     dp[0] = 1;
 
-    for(int i = 1; i <= n; i++) {
+    for(int i = 1; i <= n; i++) { //first iterate sum and then given elemnet || ordered dp
         for(int j = 1; j <= 6; j++) {
             if(i >= j) {
                 dp[i] = (dp[i] + dp[i-j]) % mod;
