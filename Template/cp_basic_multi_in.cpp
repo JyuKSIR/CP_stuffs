7df5644e05d883d375fca66478e32df99f404602
JyuKSIR <162912251+JyuKSIR@users.noreply.github.com>
2025-02-12 16:01:12 +0600

    Update cp_basic_multi_in.cpp

--- Template/cp_basic_multi_in.cpp
+++ Template/cp_basic_multi_in.cpp
@@ -1,5 +1,11 @@
 #include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
 using namespace std;
+using namespace __gnu_pbds;
+ 
+template <typename num_t>
+using ordered_set = tree<num_t, null_type, less<num_t>, rb_tree_tag, tree_order_statistics_node_update>;
 
 #define ll long long
 #define ld long double
