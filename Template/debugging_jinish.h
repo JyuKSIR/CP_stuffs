4b3c3d1dbf86af12dbacae1988aef9c0a723220b
JyuKSIR <162912251+JyuKSIR@users.noreply.github.com>
2025-02-12 19:01:32 +0600

    Create debugging_jinish.h

---
+++ Template/debugging_jinish.h
@@ -0,0 +1,72 @@
+#ifndef DEBUGGING_JINISH_H
+#define DEBUGGING_JINISH_H
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define dbg(x) cerr << __LINE__ << " no. line : " << #x << " = "; _print(x); cerr << "\n"
+
+// Print functions for basic types
+void _print(long long t) { cerr << t; }
+void _print(int t) { cerr << t; }
+void _print(string t) { cerr << t; }
+void _print(char t) { cerr << t; }
+void _print(long double t) { cerr << t; }
+void _print(double t) { cerr << t; }
+void _print(unsigned long long t) { cerr << t; }
+
+// Template functions for containers
+template <class T, class V> void _print(pair<T, V> p);
+template <class T> void _print(vector<T> v);
+template <class T> void _print(set<T> v);
+template <class T, class V> void _print(map<T, V> v);
+template <class T> void _print(multiset<T> v);
+
+// Template function implementations
+
+template <class T, class V> 
+void _print(pair<T, V> p) {
+    cerr << "{"; _print(p.first); cerr << ","; _print(p.second); cerr << "}";
+}
+
+template <class T> 
+void _print(vector<T> v) {
+    cerr << "[ ";
+    for (T i : v) {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+
+template <class T> 
+void _print(set<T> v) {
+    cerr << "[ ";
+    for (T i : v) {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+
+template <class T> 
+void _print(multiset<T> v) {
+    cerr << "[ ";
+    for (T i : v) {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+
+template <class T, class V> 
+void _print(map<T, V> v) {
+    cerr << "[ ";
+    for (auto i : v) {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+
+#endif // DEBUGGING_JINISH_H
