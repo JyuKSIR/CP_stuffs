07a79fa10b9901699dcb7d0ee6ae24116b6737ee
JyuKSIR <162912251+JyuKSIR@users.noreply.github.com>
2025-02-12 18:58:04 +0600

    Update multi_input.cpp

--- Template/multi_input.cpp
+++ Template/multi_input.cpp
@@ -1,1 +1,39 @@
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
 
+using namespace std;
+using namespace __gnu_pbds;
+
+#ifdef LOCAL
+#include "debugging_jinish.h"
+#else
+#define dbg(...) 1
+#endif
+ 
+template <typename num_t>
+using ordered_set = tree<num_t, null_type, less<num_t>, rb_tree_tag, tree_order_statistics_node_update>;
+
+#define ll long long
+#define ld long double
+
+void solve(){
+
+    
+
+}
+
+int main(){
+
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int t; cin >> t;
+
+    while(t--){
+       solve();
+    }
+
+  return 0;
+}
