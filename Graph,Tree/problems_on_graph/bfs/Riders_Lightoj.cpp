265f87b3005bd518932e41297f86604e68ead333
JyuKSIR <162912251+JyuKSIR@users.noreply.github.com>
2025-03-31 02:59:06 +0600

    Create Riders_Lightoj.cpp

---
+++ Graph,Tree/problems_on_graph/bfs/Riders_Lightoj.cpp
@@ -0,0 +1,97 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ld long double
+
+void solve(){
+
+    int n,m; cin >> n >> m;
+
+    vector<pair<int, int>> riders;
+    vector<vector<char>> tab(n, vector<char>(m));
+    vector<vector<int>> vis(n, vector<int>(m, 0));
+    vector<vector<int>> dis(n, vector<int>(m, 0));
+    vector<vector<int>> go(n, vector<int>(m, 0));
+
+    int dx[8] = {1,1,2,2,-1,-1,-2,-2};
+    int dy[8] = {2,-2,1,-1,2,-2,1,-1};
+
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            cin >> tab[i][j];
+            if(tab[i][j] != '.'){
+                riders.push_back({i,j});
+            }
+        }
+    }
+    auto val = [&](int x, int y) -> bool {
+        if(x < 0 or x >= n or y < 0 or y >= m or vis[x][y]){
+            return false;
+        }
+        return true;
+    };
+
+    auto bfs = [&](int sx, int sy,int k) -> void {
+        vector<vector<int>> loc(n, vector<int>(m, 0));
+        queue<pair<int,int>> q;
+        q.push({sx,sy});
+        vis[sx][sy] = 1;
+        go[sx][sy]++;
+
+        while(!q.empty()){
+            auto p = q.front();
+            int a = p.first;
+            int b = p.second;
+            q.pop();
+            for(int i=0; i<8; i++){
+                int cha = a + dx[i];
+                int chb = b + dy[i];
+
+                if(val(cha,chb)){
+                    vis[cha][chb] = 1;
+                    loc[cha][chb] = loc[a][b] + 1;
+                    dis[cha][chb] += (loc[cha][chb]+1)/k;
+                    go[cha][chb]++;
+                    q.push({cha,chb});
+                }
+            }
+        }
+    };
+
+    for(auto [x,y] : riders){
+        vis = vector<vector<int>>(n, vector<int>(m, 0));
+        int k = tab[x][y] - '0';
+        bfs(x,y,k);
+    }
+    
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            cout << dis[i][j] << " ";
+        }
+        cout << "\n";
+    }
+    cout << "\n";
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            cout << go[i][j] << " ";
+        }
+        cout << "\n";
+    }
+
+}
+
+int main(){
+
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int t; cin >> t;
+
+    while(t--){
+       solve();
+    }
+
+  return 0;
+}
