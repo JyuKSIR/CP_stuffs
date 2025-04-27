cc754d7dc901690df02905df4c63d78fa9ea65cf
JyuKSIR <162912251+JyuKSIR@users.noreply.github.com>
2025-04-28 01:05:40 +0600

    Create dijkstra_set.cpp

---
+++ Graph,Tree/Traversal/dijkstra_set.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int n,m,s,e; cin >> n >> m >> s >> e;
+    s--,e--;
+
+    const int inf = 1e9+10;
+
+    vector<array<int,2>> g[n];
+    vector<int> vis(n,0),dis(n,inf);
+
+
+    for(int i=0; i<m; i++){
+        int u,v,w; cin >> u >> v >> w;
+        u--,v--;
+        g[u].push_back({v,w});
+        g[v].push_back({u,w});
+    }
+
+    auto dijkstra = [&](int s,int e) -> int{
+
+        set<array<int,2>> st;
+        st.insert({0,s});
+        dis[s] = 0;
+
+        while(!st.empty()){
+
+            auto [w,p] = *st.begin();
+            st.erase(st.begin());
+            if(vis[p]) continue;
+            vis[p] = 1;
+            for(auto [c,w] : g[p]){
+                if(dis[p] + w < dis[c]){
+                    dis[c] = dis[p] + w;
+                    st.insert({dis[c],c});
+                }
+            }
+        }
+        return dis[e];
+    };
+
+    cout << dijkstra(s,e);
+
+  return 0;
+}
