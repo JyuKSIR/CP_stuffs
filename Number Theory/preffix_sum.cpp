26a33d9ae20063fac60a6bc19b28d17719467be3
JyuKSIR <162912251+JyuKSIR@users.noreply.github.com>
2025-07-19 13:15:40 +0600

    Update preffix_sum.cpp

--- Number Theory/preffix_sum.cpp
+++ Number Theory/preffix_sum.cpp
@@ -31,11 +31,6 @@ void solve(){
 
 int main(){
 
-    // #ifndef ONLINE_JUDGE
-    // freopen("input.txt", "r", stdin);
-    // freopen("output.txt", "w", stdout);
-    // #endif
-
     ios::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
