108f5736d4209c4092ca633ead7a9aa0a6b366d0
JyuKSIR <162912251+JyuKSIR@users.noreply.github.com>
2025-07-21 16:38:20 +0600

    Create bit_manipulation.cpp

---
+++ Number Theory/bit_manipulation.cpp
@@ -0,0 +1,77 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    // 2^n = (1 << n)
+
+    // printing a number in binary
+
+    int n; cin >> n;
+
+    auto print = [](int num) -> void{
+        for(int i = 30; ~i; i--){
+            cout << ((num >> i) & 1);
+        }
+    };
+
+    //print(n); // when printing( right shift ( >> ))
+
+    // given n numbers count how many set bits are there in which index
+
+    int ar[n], ct[30]{};
+
+    for(int i=0; i<n; i++) cin >> ar[i];
+
+    for(int i = 0; i < n; i++){
+        for(int j = 0 ; j < 30 ; j++){
+            if((ar[i] & (1 << j))) ct[j]++; //(left shift)
+        }
+    }
+
+    for(int i = 0 ; i < 30 ; i++){
+        cout << i << " " << ct[i] << "\n";
+    }
+
+    // set 6th bit of all n numbers (or, left shift)
+
+    for(int i = 0; i < n; i++){
+        (ar[i] |= (1 << 6));
+    }
+    int ct2[30]{};
+
+    for(int i = 0; i < n; i++){
+        for(int j = 0 ; j < 30 ; j++){
+            if((ar[i] & (1 << j))) ct2[j]++;
+        }
+    }
+
+    for(int i = 0 ; i < 30 ; i++){
+        cout << i << " " << ct2[i] << "\n";
+    }
+
+    // unset 6th bit of all n numbers (and,left shift)
+
+    for(int i = 0; i < n; i++){
+        (ar[i] &= ~(1 << 6));
+    }
+
+    int ct2[30]{};
+
+    for(int i = 0; i < n; i++){
+        for(int j = 0 ; j < 30 ; j++){
+            if((ar[i] & (1 << j))) ct2[j]++;
+        }
+    }
+
+    for(int i = 0 ; i < 30 ; i++){
+        cout << i << " " << ct2[i] << "\n";
+    }
+    // toggle 6th bit of all n numbers
+
+    for(int i = 0; i < n; i++){
+        (ar[i] ^= (1 << 6));
+    }
+
+  return 0;
+}
