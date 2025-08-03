a121a9d24d043c964e75194c0eedd1001f4c4beb
JyuKSIR <162912251+JyuKSIR@users.noreply.github.com>
2025-08-04 00:36:51 +0600

    Add files via upload

---
+++ Number Theory/Problems/sieve,prime factorization/medium_factorization.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define ld long double
+
+using namespace std;
+
+int main(){
+
+  const int N = 1e7 + 5;
+  vector<int> vis(N,1), lp(N,-1);
+
+  vis[0] = vis[1] = 0;
+
+  for(int i = 2; i < N; i++){
+    if(vis[i]){
+      lp[i] = i;
+      for(int j = 2 * i; j < N; j += i){
+        vis[j] = 0;
+        if(lp[j] == -1) lp[j] = i;
+      }
+    }
+  }
+
+  int n;
+  while (scanf("%d", &n) != EOF) {
+    vector<int> pf;
+    while(n > 1){
+      int prf = lp[n];
+      while(n % prf == 0){
+        pf.push_back(prf);
+        n /= prf; 
+      }
+    }
+    printf("1");
+    for(auto x : pf) printf(" x %d", x);
+    printf("\n");
+  }
+  
+ return 0;
+}
