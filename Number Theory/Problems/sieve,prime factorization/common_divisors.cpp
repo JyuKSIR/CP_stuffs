fe30a55e464095c243d18b8c80c15a8df6d8a8dd
JyuKSIR <162912251+JyuKSIR@users.noreply.github.com>
2025-08-06 22:36:29 +0600

    Add files via upload

---
+++ Number Theory/Problems/sieve,prime factorization/common_divisors.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define ld long double
+ 
+using namespace std;
+
+const ll x = 1e6+10;
+vector<ll> fq(x,0), ct(x,0);
+ 
+int main(){
+ 
+  ll n; scanf("%lld", &n);
+  for(ll i = 0; i < n; i++){
+    ll p; scanf("%lld", &p);
+    fq[p]++;
+  }
+ 
+  for(ll i = 2; i < x; i++){
+    for(ll j = i; j < x; j += i){
+      ct[i] += fq[j];
+    }
+  }
+ 
+  ll mx = 1;
+ 
+  for(ll i = 1; i < x; i++){
+    if(ct[i] >= 2){
+      mx = max(mx, i);
+    }
+  }
+ 
+  printf("%lld", mx);
+ 
+ return 0;
+}
