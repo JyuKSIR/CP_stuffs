7551bf59f9678c8c017fac5a2526d68b37b1310d
JyuKSIR <162912251+JyuKSIR@users.noreply.github.com>
2025-08-09 16:00:30 +0600

    Create minSubstring.cpp

---
+++ TP,SW/minSubstring.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define ld long double
+
+using namespace std;
+
+int main(){
+
+    string s; cin >> s;
+    int n = (int)s.size();
+    unordered_set<char> us(s.begin(), s.end());
+    int sz = (int)us.size();
+    int need[26]{};
+    for (auto c : us) {
+        need[c - 'a'] = 1;
+    }
+    int have[26]{};
+    int i = 0, j = 0, mn = INT_MAX, in = -1, ct = 0;
+    while (j < n) {
+        have[s[j] - 'a']++;
+        if (have[s[j] - 'a'] == 1 && need[s[j] - 'a']) ct++;
+        while (ct == sz) {
+            if (j - i + 1 < mn) {
+                mn = j - i + 1;
+                in = i;
+            }
+            have[s[i] - 'a']--;
+            if (have[s[i] - 'a'] == 0 && need[s[i] - 'a']) ct--;
+            i++;
+        }
+        j++;
+    }
+
+    printf("%d ", mn);
+    return 0;
+}
