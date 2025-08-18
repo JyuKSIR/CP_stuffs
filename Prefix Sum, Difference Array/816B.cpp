5e1fdc7cc343f65c6322d7fdf6af46b7da61acca
JyuKSIR <162912251+JyuKSIR@users.noreply.github.com>
2025-08-18 19:12:02 +0600

    Add files via upload

---
+++ Prefix Sum, Difference Array/816B.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define ld long double
+
+using namespace std;
+
+int main(){
+
+
+  const int N = 2e5+5;
+  int n,k,q;
+  scanf("%d%d%d", &n, &k, &q);
+  int da[N]{}, ans[N]{};
+  for(int i = 1; i <= n; i++){
+    int l,r;
+    scanf("%d%d", &l, &r);
+    da[l]++;
+    da[r+1]--;
+  }
+
+  partial_sum(da,da+N,da);
+
+  for(int i = 1; i <= N; i++){
+    if(da[i] >= k) ans[i]++;
+  }
+
+  partial_sum(ans,ans+N,ans);
+
+  for(int i = 0; i < q; i++){
+    int l,r;
+    scanf("%d%d", &l, &r);
+    printf("%d\n", ans[r] - ans[l-1]);
+  }
+
+
+ return 0;
+}
