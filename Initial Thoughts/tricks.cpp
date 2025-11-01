f6ce04e8d1e431450c310c0c9d4be55af4c501f7
JyuKSIR <162912251+JyuKSIR@users.noreply.github.com>
2025-11-01 21:15:01 +0600

    Update tricks.cpp

--- Initial Thoughts/tricks.cpp
+++ Initial Thoughts/tricks.cpp
@@ -1,1 +1,2 @@
 // whenever we see a problem like partitioing a array into k subarrays and find something first think about DP
+// maximize of minimum of something (max min) or minimize of maximum of something (min max) can be solved by BINARY SEARCH
