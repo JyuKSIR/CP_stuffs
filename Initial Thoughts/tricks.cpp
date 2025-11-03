877d8bb4ebd5563863bb5e85a8c19b2c27d49ae2
JyuKSIR <162912251+JyuKSIR@users.noreply.github.com>
2025-11-03 13:48:55 +0600

    Update tricks.cpp

--- Initial Thoughts/tricks.cpp
+++ Initial Thoughts/tricks.cpp
@@ -1,2 +1,7 @@
-// whenever we see a problem like partitioing a array into k subarrays and find something first think about DP
-// maximize of minimum of something (max min) or minimize of maximum of something (min max) can be solved by BINARY SEARCH
+***** DP *****
+1. whenever we see a problem like partitioing a array into k subarrays and find something first think about DP
+  
+***** BINARY SEARCH ***** 
+1. maximize of minimum of something (max min) or minimize of maximum of something (min max) can be solved by BINARY SEARCH
+2. in BS you have to always try to think about how you can play with the mid value cause the mid value is always changing else everything remains same 
+
