c0559b5728bb478c173c50459ab37a54d5a0937e
JyuKSIR <162912251+JyuKSIR@users.noreply.github.com>
2025-11-05 16:42:12 +0600

    Update tricks.cpp

--- Initial Thoughts/tricks.cpp
+++ Initial Thoughts/tricks.cpp
@@ -5,3 +5,5 @@
 1. maximize of minimum of something (max min) or minimize of maximum of something (min max) can be solved by BINARY SEARCH
 2. in BS you have to always try to think about how you can play with the mid value cause the mid value is always changing else everything remains same 
 
+***** Prefix Sum *****
+1. in a array of size N there are N*(N-1)/2 segments exist if we count 0 it will be 1 extra
