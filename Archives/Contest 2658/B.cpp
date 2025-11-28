761b37fe7a9da1abd7f897f93e4220e63343d496
JyuKSIR <162912251+JyuKSIR@users.noreply.github.com>
2025-11-29 00:28:38 +0600

    Update B.cpp

--- Archives/Contest 2658/B.cpp
+++ Archives/Contest 2658/B.cpp
@@ -1,1 +1,26 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define ull unsigned long long
 
+using namespace std;
+
+int main() {
+  cin.tie(0)->sync_with_stdio(0);
+
+  int n; cin >> n;
+  int x = 1000, y = 1000, z = 1000;
+  while(n--){
+    int ar[3];
+    for(int i = 0; i < 3; i++){
+      cin >> ar[i];
+    }
+    sort(ar, ar + 3);
+    x = min(x, ar[0]);
+    y = min(y, ar[1]);
+    z = min(z, ar[2]);
+  }
+  cout << x * y * z;
+  
+  return 0;
+}
+// https://qoj.ac/contest/2658/problem/15408
