6d64cf9c41139e2de607138c55fe40e67fcbf3bb
JyuKSIR <162912251+JyuKSIR@users.noreply.github.com>
2026-02-13 20:39:24 +0600

    Create formulas.cpp

---
+++ Initial Thoughts/Modulo/formulas.cpp
@@ -0,0 +1,3 @@
+-- > (a + b) % x can be written by a % x = (x - b % x) % x
+here we can minus b from x because we want a positive ans. and we always do overall mod % x because b itself can be equal to x;
+what will happen then? take the values and do math
