c2b784052118073a97296085d717916e05da4841
JyuKSIR <162912251+JyuKSIR@users.noreply.github.com>
2026-02-20 20:32:12 +0600

    Update digit_sum.cpp

--- Recursion/digit_sum.cpp
+++ Recursion/digit_sum.cpp
@@ -1,1 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main(){
+  cin.tie(0)->sync_with_stdio(0);
+
+  int n; cin >> n;
+  function<int(int)> ds = [&](int n){
+    if(n == 0) return 0;
+    return ds(n / 10) + n % 10;
+  };
+  cout << ds(n);
+}
 
