90b475898bc1a4c53897ec106d900cbaadff9c04
JyuKSIR <162912251+JyuKSIR@users.noreply.github.com>
2026-02-20 20:36:03 +0600

    Update Array_sum.cpp

--- Recursion/Array_sum.cpp
+++ Recursion/Array_sum.cpp
@@ -1,1 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main(){
+  cin.tie(0)->sync_with_stdio(0);
+  
+  int n; cin >> n;
+  int a[n];
+  for(int i = 0; i < n; i++){
+    cin >> a[i];
+  }
+  function<int(int)> as = [&](int n){
+    if(n < 0) return 0;
+    return as(n - 1) + a[n];
+  };
+  cout << as(n - 1);
+}
 
