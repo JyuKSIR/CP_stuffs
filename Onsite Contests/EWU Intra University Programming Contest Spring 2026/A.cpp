fca76628ad363d9e615928745acc5596719478e0
JyuKSIR <162912251+JyuKSIR@users.noreply.github.com>
2026-04-12 02:06:33 +0600

    Create A.cpp

---
+++ Onsite Contests/EWU Intra University Programming Contest Spring 2026/A.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main(){
+	cin.tie(0)->sync_with_stdio(0);
+	cout << "E\n";
+	cout << "EW\n";
+	cout << "EWU\n";
+	cout << "EWUP\n";
+	cout << "EWUPS\n";
+	cout << "EWUPSC\n";
+	cout << "EWUPSC\n";
+	cout << "EWUPS\n";
+	cout << "EWUP\n";
+	cout << "EWU\n";
+	cout << "EW\n";
+	cout << "E\n";
+}
